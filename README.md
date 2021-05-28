cfe58e09864cfdf0971015d7f033f5a27b1d640e
Nelesh Singla <nsingla@brandwatch.com>
2021-05-28 12:36:35 -0400

    Adding javadoc and more info to readme

--- README.md
+++ README.md
@@ -9,4 +9,16 @@ The purpose of this test is 3 fold:
 * Read the api docs: https://gender-api.com/en/api-docs
 * Think about how you want test the end points via automation tests
 
-**NOTE**: You are free to use any language you are comfortable with, but for reference, we've provided a maven project using Jackson and RestAssured Libraries that you can use. 
+**NOTE**: You are free to use any language you are comfortable with, but for reference, we've provided a maven project using Jackson and RestAssured libraries that you can use (feel free to make any changes that you see fit)
+
+## Using the example test framework
+* `RestClient.java` simplifies the way you make a REST call using RestAssured library, just create an instance of RestClient and call utility methods such as `get` `post` to make call to an endpoint
+* You can write test in either junit4 or junit5, junit5 example is in `PostsTests.java`.
+
+## How will we rate it
+Our team will check following:
+* Scenarios Covered
+* Your understanding of the problem
+* Coding Style
+* Readability
+ 
--- src/test/java/jsonplaceholder/PostTests.java
+++ src/test/java/jsonplaceholder/PostTests.java
@@ -2,12 +2,17 @@ package jsonplaceholder;
 
 import client.RestClient;
 import dtos.jsonplaceholder.Post;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
 import java.util.List;
 
+/**
+ * An example Test class describing how to use the framework to write tests
+ */
 public class PostTests {
     private static final Logger logger = LoggerFactory.getLogger(PostTests.class);
 
@@ -17,10 +22,21 @@ public class PostTests {
 
     private RestClient client = new RestClient();
 
+    /**
+     * Make a call to <strong>/posts</strong> endpoint and print all returned posts
+     */
     @Test
     public void getPosts() {
         List<Post> posts = client.get(POSTS_ENDPOINT, List.class);
         logger.info(() -> "Posts: " + posts);
+    }
 
+    /**
+     * Make a call to <strong>/posts</strong> endpoint and verify that the status code in the response is <strong>200</strong>
+     */
+    @Test
+    public void getPostsWithAssertion() {
+        Response response = client.get(POSTS_ENDPOINT, Response.class);
+        Assertions.assertEquals(200, response.getStatusCode(), "Expected Status Code Failure");
     }
 }
